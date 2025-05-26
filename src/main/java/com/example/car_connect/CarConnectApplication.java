abb522a552ee6b6dbfeab85bb3b91d16fba21d46
Alym2004 <alymyrysmamatov@gmail.com>
2025-05-26 22:36:07 +0600

    feat: create main Spring Boot application class

---
+++ src/main/java/com/example/car_connect/CarConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarConnectApplication.class, args);
+	}
+
+}
